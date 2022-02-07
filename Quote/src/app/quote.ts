84ea0a38fbb056f94af32aa9faee7dbf820c4776
Owilly <pasaclouma54@gmail.com>
2022-02-07 17:40:51 +0300

    add quotes

--- Quote/src/app/quote.ts
+++ Quote/src/app/quote.ts
@@ -3,6 +3,8 @@ export class Quote {
   // coolQuote: string;
   // quoteDetails: string;
 
+
+  
     showDescription: boolean;
     constructor(public author: string,public coolQuote: string,public description: string){
       this.showDescription=false;
